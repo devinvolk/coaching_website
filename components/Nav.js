2f048dd85ce520440d4e3fca30370857ba4d4412
devinvolk@comcast.net <devinvolk@comcast.net>
2024-01-16 09:24:52 -0800

    mobile nav added, styled, and complete

--- components/Nav.js
+++ components/Nav.js
@@ -2,10 +2,14 @@
 
 import Link from 'next/link';
 import React, {useState, useEffect} from 'react'
+import { AiOutlineClose } from 'react-icons/ai';
+import { GiHamburgerMenu } from 'react-icons/gi';
 
 const Nav = () => {
     const [navVisible, setNavVisible] = useState(false);
     const [isActive, setIsActive] = useState('//');
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const [isScrolled, setIsScrolled] = useState(false);
 
     const NavItems = [
         {
@@ -35,25 +39,52 @@ const Nav = () => {
     ]
 
     const handleClick = (page) => {
-        setIsActive(page)
+        setIsActive(page);
+    }
+
+    const toggleMenu = () => {
+      setIsMenuOpen(!isMenuOpen);
+    }
+
+    const scrolling = () => {
+      if (window.scrollY >= 1) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
     }
 
     useEffect(() => {
       const delay = setTimeout(() => {
         setNavVisible(true);
-      }, 500);
+      }, 300);
     
       return () => {
         clearTimeout(delay);
       }
     }, [])
+
+    useEffect(() => {
+      window.addEventListener('scroll', scrolling);
+    
+      return () => {
+        window.removeEventListener('scroll', scrolling);
+      }
+    }, [])
+    
+    useEffect(() => {
+      if (isMenuOpen) {
+        document.body.style.overflow = 'hidden'
+      } else {
+        document.body.style.overflow = 'auto'
+      }
+    }, [isMenuOpen])
     
 
-
   return (
-    <nav className={`fixed z-50 h-14 py-2 md:py-4 md:h-20 w-full ${navVisible ? 'opacity-100 transition-opacity duration-1000' : 'opacity-0'}`}>
+    <nav className={`fixed z-50 h-14 py-2 md:py-4 md:h-20 w-full ${isMenuOpen ? 'bg-white shadow-md' : (isScrolled ? 'bg-white shadow-md' : 'bg-transparent')}`}>
         {/* desktop nav */}
-        <div className='flex flex-row justify-end space-x-6 mr-4'>
+        <div className={`hidden md:flex flex-row justify-end space-x-8 mr-6 ${navVisible ? 'opacity-100 transition-opacity duration-1000' : 'opacity-0'}`}>
             {NavItems.map((item, index) => 
             item.title === 'Social' ? (
             <a
@@ -75,6 +106,32 @@ const Nav = () => {
                   {item.title}
               </Link>
             ))}
+        </div>
+
+        {/* mobile nav */}
+        <div className='md:hidden flex justify-end mr-4'>
+          <button>
+            {isMenuOpen ? (
+            <AiOutlineClose
+            color={isMenuOpen ? 'black' : (isScrolled ? 'black' : 'white')}
+            size={30}
+            onClick={toggleMenu}
+            />) : (
+            <GiHamburgerMenu
+            color={isScrolled ? 'black' : 'white'}
+            size={30}
+            onClick={toggleMenu}
+            />)}
+          </button>
+        </div>
+
+        {/* mobile drop down */}
+        <div className={`${isMenuOpen ? 'mr-0' : 'mr-[-240px]'} transition-[margin-right] ease-in-out duration-500 fixed flex flex-col bottom-0 top-12 right-0 w-60 z-40 bg-white`}>
+              {NavItems.map((item, index) => (
+                <Link key={index} href={item.page} onClick={toggleMenu} className='text-2xl font-bold text-slate-950 mx-auto mt-14'>
+                  {item.title}
+                </Link>
+              ))}
         </div>
     </nav>
   )
