439b7cd6a184fc6b2445ebaeb45e2160601dc1d0
devinvolk@comcast.net <devinvolk@comcast.net>
2024-01-17 12:55:22 -0800

    Styled the main section of the home page

--- app/page.js
+++ app/page.js
@@ -22,37 +22,64 @@ export default function Home() {
             <Image
               src={finish}
               alt="Devin Volk carrying a female competitor across the finish line at the 2022 Ironman World Championships in Kona after he had finished"
-              style={{ maxHeight: '650px', maxWidth: '473px'}}
+              style={{ maxHeight: "650px", maxWidth: "473px" }}
             />
           </div>
           <div className="lg:col-start-4 lg:col-end-7">
-            <h2 className="text-3xl italic semi-bold text-center mb-4">About Devin</h2>
+            <h2 className="text-3xl italic semi-bold text-center mb-4">
+              About Devin
+            </h2>
             <p className="text-center text-[#626262]">
-              Born in Bergen, Norway in 1996, Gustav won his first national
-              titles in 2014, winning both the triathlon and duathlon, and went
-              on to score a top 10 finish in the Junior World Championships the
-              following year. After a testing transition into the world of Elite
-              racing, Gustav found consistency in 2017 and a first World Cup
-              gold in Karlovy Vary thanks to a brilliant ride and run. <br /><br /> The
-              2018 season was certainly one to remember, opening with a top 10
-              finish in Abu Dhabi before landing bronze in Bermuda and sharing
-              the podium with his teammates, the first time a country had swept
-              the men's WTS podium. World Cup wins in Lausanne and Weihai with a
-              second-place finish at the Ironman 70.3 Middle East Championships
-              in Bahrain cemented the young Norwegian's reputation as one to
-              watch for 2019. <br /><br /> Gustav certainly didn't disappoint with a third
-              place at the ITU World Triathlon Bermuda and fourth place finish
-              at the ITU World Triathlon Grand Final in Lausanne but his biggest
-              moment of his career undoubtedly came at the 2019 Ironman 70.3
-              World Championships in Nice where he beat a truly world class
-              field to become the youngest ever Ironman World Champion. Gustav
-              then followed this victory up by winning the 2021 Ironman 70.3
-              World Championships and at the same time was crowned the world
-              no.1 triathlete. Gustav stamped his name in the history books by
-              winning the 2022 Ironman World Championships in Kona, Hawaii.
+              Born in Corvallis, Oregon in 1994, Devin grew up participating
+              primarily in team sports. In high school, he was a 4 sport varisty
+              athlete and accumilated 10 varisty letters in Soccer, Cross
+              Country, Alpine Ski Racing, and Track. After high school, he
+              attending California Polytechnic State University and this is
+              where he first started triathlon.
+              <br />
+              <br />
+              As part of the Cal Poly Triathlon Team, Devin quickly feel in love
+              with the sport and started climbing his way up the ladder. By the
+              end of his first season, he was ranked second on the team and in
+              the top 150 in the nation. He finished his collegiate carrer with
+              a 3rd overall combined finish at the 2018 USAT Collegiate National
+              Championships.
+              <br />
+              <br />
+              Devin went on to represent the United States at 4 amatuer Ironman
+              70.3 World Championships and achieved a World Championship Title
+              in 2021. In 2022 Devin had the incredible opportunity to train
+              with Chelsea Sodaro during her build up to her world title. This
+              experience allowed him first hand access to the routine, mindset,
+              and determination of a World Champion and was undoubtidly a strong
+              contributing factor to his own succuess on the island. Devin
+              finished his amatuer career as the runner up in the 25-29
+              age group in Hawaii and had one of the most consistent
+              performances across all three disciplines of any finisher.
+              <br />
+              <br />
+              Devin was also a part the viral events that unfolded at the finish
+              line of the 2022 Ironman World Championships. To read more about
+              what happened{" "}
+              <span>
+                <a
+                  href="https://www.slowtwitch.com/Interview/Be_Like_Devin_Volk_8562.html"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="font-bold text-neutral-800 underline"
+                >
+                  Click Here!
+                </a>
+              </span>
             </p>
           </div>
         </div>
+        <hr className="my-10 border-t-2 border-neutral-500 rounded-full" />
+        <h2 className="text-3xl text-center font-semibold w-4/5 mx-auto text-neutral-900">
+          Devin is now racing as a professional and has achieved 2 top 10
+          performances in just his first season.
+        </h2>
+        <hr className="my-10 border-t-2 border-neutral-500 rounded-full" />
       </div>
     </div>
   );
