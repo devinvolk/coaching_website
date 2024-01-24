f3612b6c9d5bb5357041f7fec63298b1a6198fba
devinvolk@comcast.net <devinvolk@comcast.net>
2024-01-24 12:40:58 -0800

    rewrote the main bio

--- app/page.js
+++ app/page.js
@@ -31,37 +31,40 @@ export default function Home() {
               About Devin
             </h2>
             <p className="text-neutral-700">
-              Born in Corvallis, Oregon in 1994, Devin grew up participating
-              primarily in team sports. In high school, he was a 4 sport varisty
-              athlete and accumilated 10 varisty letters in Soccer, Cross
-              Country, Alpine Ski Racing, and Track. After high school, he
-              attending California Polytechnic State University and this is
-              where he first started triathlon.
+              Born in Corvallis, Oregon in 1994, Devin's early years were
+              defined by active participation in team sports. Throughout high
+              school, he excelled as a four-sport varsity athlete, accumulating
+              an impressive 10 varsity letters in Soccer, Cross Country, Alpine
+              Ski Racing, and Track. Post high school, he pursued his education
+              at California Polytechnic State University, where his journey into
+              triathlon began.
               <br />
               <br />
-              As part of the Cal Poly Triathlon Team, Devin quickly feel in love
-              with the sport and started climbing his way up the ladder. By the
-              end of his first season, he was ranked second on the team and in
-              the top 150 in the nation. He finished his collegiate carrer with
-              a 3rd overall combined finish at the 2018 USAT Collegiate National
-              Championships.
+              As a member of the Cal Poly Triathlon Team, Devin swiftly
+              developed a passion for the sport, steadily advancing in skill and
+              rank. By the conclusion of his inaugural season, he secured the
+              second spot on the team and claimed a position within the top 150
+              nationally. His collegiate career reached its pinnacle with a
+              notable 3rd overall combined finish at the 2018 USAT Collegiate
+              National Championships.
               <br />
               <br />
-              Devin went on to represent the United States at 4 amatuer Ironman
-              70.3 World Championships and achieved a World Championship Title
-              in 2021. In 2022 Devin had the incredible opportunity to train
-              with Chelsea Sodaro during her build up to her world title. This
-              experience allowed him first hand access to the routine, mindset,
-              and determination of a World Champion and was undoubtidly a strong
-              contributing factor to his own succuess on the island. Devin
-              finished his amatuer career as the runner up in the 25-29 age
-              group in Hawaii and had one of the most consistent performances
-              across all three disciplines of any finisher.
+              Devin's commitment extended beyond collegiate competitions; he
+              proudly represented the United States in four amateur Ironman 70.3
+              World Championships, culminating in a World Championship Title in
+              2021. In 2022, Devin seized a unique opportunity to train
+              alongside Chelsea Sodaro during her preparation for a world title.
+              This immersive experience provided him with invaluable insights
+              into the routine, mindset, and determination of a World Champion,
+              undoubtedly influencing his own success on the island. His amateur
+              career concluded with a remarkable runner-up placement in the
+              25-29 age group in Hawaii, showcasing consistent performances
+              across all three disciplines.
               <br />
               <br />
-              Devin was also a part the viral events that unfolded at the finish
-              line of the 2022 Ironman World Championships. To read more about
-              what happened{" "}
+              Notably, Devin played a part in the viral events unfolding at the
+              finish line of the 2022 Ironman World Championships. For further
+              details on these events{" "}
               <span>
                 <a
                   href="https://www.slowtwitch.com/Interview/Be_Like_Devin_Volk_8562.html"
